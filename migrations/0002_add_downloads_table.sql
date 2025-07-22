23c25e69c261c3b4706a106e79d3d45b39c4e1f2
sadeq-qafari <sadeq.qafari@gmail.com>
2025-07-22 23:28:47 +0330

    fix: update migration

--- migrations/0002_add_downloads_table.sql
+++ migrations/0002_add_downloads_table.sql
@@ -1,9 +1,10 @@
 -- Migration number: 0001 	 2025-06-19T18:13:02.648Z
-CREATE TABLE IF NOT EXISTS tasks (
+CREATE TABLE IF NOT EXISTS downloads (
     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    name TEXT NOT NULL,
-    slug TEXT NOT NULL,
-    description TEXT NOT NULL,
-    completed INTEGER NOT NULL,
-    due_date DATETIME NOT NULL
+    url TEXT NOT NULL,
+    status TEXT NULL,
+    metadata TEXT NULL,
+    create_date DATETIME NOT NULL,
+    start_date DATETIME NOT NULL,
+    end_date DATETIME NOT NULL,
 );
